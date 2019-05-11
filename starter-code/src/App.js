ef78cab6592cec70e0ed9f3dfdc0cf91f9be200e
Guido <Guido@mac.local>
2019-05-11 16:39:48 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,98 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import contacts from "./contacts.json";
 
 class App extends Component {
+  state = {
+    contacts: contacts.slice(0, 5)
+  };
+
+  getRandomActor = () => {
+    // add random actor
+    let randomActor = contacts[Math.floor(Math.random() * contacts.length)];
+    contacts: this.state.contacts.push(randomActor);
+    this.setState({});
+  };
+
+  filterByName = () => {
+    // filter by name
+    let contactsByName = this.state.contacts.sort((a, b) => {
+      return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    });
+    this.setState({
+      contacts: contactsByName
+    });
+  };
+
+  filterByPopularity = () => {
+    // filter by popularity
+    let contactsByPopularity = this.state.contacts.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    this.setState({
+      contacts: contactsByPopularity
+    });
+  };
+
+  removeActor = (index) => {
+
+   let contactMinusOne = this.state.contacts
+   contactMinusOne.splice(index, 1 )
+
+    this.setState({
+      contacts: contactMinusOne
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="my-section">
+        <h1>IronContacts</h1>
+        <div className="filter-buttons">
+          <button onClick={this.getRandomActor}>Add random contact</button>
+          <button onClick={this.filterByName}>Sort by name</button>
+          <button onClick={this.filterByPopularity}>Sort by popularity</button>
+        </div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((actor, index) => (
+
+              <tr>
+              <td>
+                
+                 <img src={actor.pictureUrl} />
+               
+              </td>
+              <td>
+                {actor.name}
+              </td>
+              <td>
+                {actor.popularity}
+              </td>
+              <td>
+                <button onClick={() => this.removeActor(index)}>Delete</button>
+              </td>
+            </tr>
+            ))}
+            
+          </tbody>
+        </table>
+      </section>
     );
   }
 }
 
+
+
 export default App;
+
